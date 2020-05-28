70582bb8a8ad26e5b6dae35bc99efc5754231899
libertininick <libertini.nick@gmail.com>
2020-05-28 16:34:40 -0600

    Add bounding box functions

--- scripts/utils.py
+++ scripts/utils.py
@@ -5,8 +5,171 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import numpy as np
 from PIL import Image
+import scipy.sparse
 
 
+def load_sparse_matrix(path):
+    sparse_matrix = scipy.sparse.load_npz(path)
+    arr = sparse_matrix.todense()
+    return arr
+
+
+#region mAP
+def iou(box_a, box_b):
+    """Calculate IoU of two bouding boxes
+    
+    Args:
+        box_a (list): [xmin, ymin, width, height]
+        box_b (list): [xmin, ymin, width, height]
+    
+    Returns:
+        float: value of the IoU for the two boxes.
+    
+    """
+    # Box a
+    x1_a, y1_a, w_a, h_a = box_a
+    x2_a = x1_a + w_a
+    y2_a = y1_a + h_a
+    
+    # Box b
+    x1_b, y1_b, w_b, h_b = box_b
+    x2_b = x1_b + w_b
+    y2_b = y1_b + h_b
+    
+    # No overlap check
+    if x2_a < x1_b:
+        return 0
+    elif x2_b < x1_a:
+        return 0
+    elif y2_a < y1_b:
+        return 0
+    elif y2_b < y1_a:
+        return 0
+    
+    # Box areas
+    area_a = (x2_a - x1_a + 1) * (y2_a - y1_a + 1)
+    area_b = (x2_b - x1_b + 1) * (y2_b - y1_b + 1)
+
+    # Intersection 
+    x1_max = max(x1_a, x1_b)
+    x2_min = min(x2_a, x2_b)
+    y1_max = max(y1_a, y1_b)
+    y2_min =min(y2_a, y2_b)
+    area_i = (x2_min - x1_max + 1) * (y2_min - y1_max + 1)
+    
+    iou = area_i / (area_a + area_b - area_i)
+    
+    return iou
+
+def iou_pairs(boxes_pred, boxes_true):
+    """Calculates the IoU between every pair of predictions and ground truth objects
+    
+    Args:
+        boxes_pred (ndarray): (n_predictions, 4)
+        boxes_true (ndarray): (n_objects, 4)
+        
+    Returns:
+        ious (ndarray): (n_predictions, n_objects)
+        
+    """
+    
+    ious = [[iou(bb_p, bb_t) for bb_t in boxes_true] for bb_p in boxes_pred]
+    return np.array(ious)
+
+def precision(ious, confidence_levels=None, iou_threshold=0.5):
+    """Calculates the prediction precision for a specific IoU threshold
+    
+    Args:
+        ious (ndarray): (n_predictions, n_objects)
+        confidence_levels (list): Confidence level between 0 and 1 for each prediction
+        iou_threshold (float): value of IoU to consider as threshold for a true prediction.
+    
+    Returns:
+        precision (float): TP(t) / (TP(t) + FP(t) + FN(t))
+    """
+
+    # Order predictions by confidence (most -> least)
+    if confidence_levels is not None: 
+        ious = ious[np.argmax(-np.array(confidence_levels)), :]
+        
+    object_idxs = np.arange(ious.shape[1], dtype=np.uint8)
+    assigned_objects = np.array([], dtype=np.uint8)
+    
+    # Iterate across predictions counting TPs and FPs
+    tp, fp = 0, 0
+    for predicition_ious in ious:
+        
+        unassigned_idxs = np.setdiff1d(object_idxs, assigned_objects)
+        
+        if len(unassigned_idxs) > 0:
+            predicition_ious = predicition_ious[unassigned_idxs]
+            iou_max = np.argmax(predicition_ious)
+            
+            if predicition_ious[iou_max] >= iou_threshold:
+                tp += 1
+                assigned_objects = np.append(assigned_objects, unassigned_idxs[iou_max])
+            else:
+                fp += 1
+        else:
+            fp += 1
+
+    # Count left over objects and FNs
+    fn = len(object_idxs) - len(assigned_objects)
+
+    return tp/(tp + fp + fn)
+
+
+def average_precision( boxes_pred
+                      , boxes_true
+                      , confidence_levels=None
+                      , iou_thresholds=np.arange(50, 80, 5)/100
+                     ):
+    """Calculates the average prediction precision for a range IoU thresholds
+    
+    Args:
+        boxes_pred (ndarray): (n_predictions, 4)
+        boxes_true (ndarray): (n_objects, 4)
+        confidence_levels (list): Confidence level between 0 and 1 for each prediction
+    
+    Returns:
+        avg_precision (float)
+    """
+        
+    ious = iou_pairs(boxes_pred, boxes_true)
+    precisions = [precision(ious, confidence_levels, t) for t in iou_thresholds]
+    avg_precision = np.mean(precisions)
+    
+    return avg_precision
+
+
+def mAP(y_pred, y_true, iou_thresholds=np.arange(50, 80, 5)/100):
+    """Calculates the mean average prediction precision for set of images
+    
+    Args:
+        y_pred (list): List of predictions for each image
+                       Each image should have an array of 
+                       bounding box predicitions including 
+                       prediction confidences
+                       [confidence, xmin, ymin, width, height]
+        y_true (list): List of bounding boxes for each image
+                       [xmin, ymin, width, height]
+        iou_thresholds (list): Range of IoU thresholds for average precision calc
+        
+    Returns:
+        mean_avg_precision (float)
+    """
+
+    avg_precisions = [average_precision(y_p[:,1:], y_t, y_p[:,0], iou_thresholds) 
+                      for y_p, y_t 
+                      in zip(y_pred, y_true)
+                     ]
+    mean_avg_precision = np.mean(avg_precisions)
+    
+    return mean_avg_precision
+#endregion
+
+
+#region Bounding Boxes
 def get_bbs(df_summary, image_id):
     """Gets the bounding boxes for an image
 
@@ -30,69 +193,11 @@ def slice_to_bb(im, bb):
     return im[i:i+h, j:j+w]
 
 
-def binarize(im):
-    """Binarize an image into background (0) and foreground (1) using Otsu's method
-    """
-    im_gray = cv2.cvtColor(im, cv2.COLOR_RGB2GRAY)
-    _, im_binary = cv2.threshold(im_gray, 0, 255, cv2.THRESH_OTSU)
-    return im_binary
-
-
-def remove_noise(im, kernel=np.ones((3,3), np.uint8), iterations=2):
-    """Remove noise in a binary image via morphological transformations
-    """
-    im_denoise = cv2.morphologyEx(im, cv2.MORPH_OPEN, kernel, iterations)
-    im_denoise = cv2.morphologyEx(im_denoise, cv2.MORPH_CLOSE, kernel, iterations)
-    return im_denoise
-
-
-def label_largest_component(im):
-    """Labels largest connected foreground component
-    """
-    labels = np.zeros_like(im, np.uint8)
-
-    # Run connected components on foreground
-    _, components = cv2.connectedComponents(im)
-    comp_ids, comp_counts = np.unique(components, return_counts=True)
-    
-    if len(comp_ids) > 1:
-        biggest_comp = comp_ids[1:][np.argmax(comp_counts[1:])]
-        labels[components == biggest_comp] = 1
-
-    return labels
-
-
-def bbs_to_segmentation_mask(im, bbs):
-    """Converts instance bounding boxes in an image to a single segmentation mask
-
-    Args:
-        im (ndarray): Numpy array of image (colors, height, width)
-    """
-
-    # Initialize mask with all 0s
-    mask = np.zeros(im.shape[:2])
-    
-    for bb in bbs:
-        # Fill in portion of mask associated with bb segment
-        j, i, w, h = bb
-        
-        im_slice = im[i:i+h, j:j+w]
-        im_slice = binarize(im_slice)
-        im_slice = remove_noise(im_slice)
-        
-        mask[i:i+h, j:j+w] += label_largest_component(im_slice)
-    
-    # Trunc overlapping segments
-    mask = np.minimum(1, mask)
-    
-    return mask
-
-
-def draw_bb(ax, bb):
+def draw_bb(ax, bb, color='r'):
     """Draws bounding box on an image
     """
     *cords, w, h = bb
-    rect = patches.Rectangle(cords, w, h, linewidth=2, edgecolor='r', facecolor='none')
+    rect = patches.Rectangle(cords, w, h, linewidth=2, edgecolor=color, facecolor='none')
     ax.add_patch(rect)
 
 
@@ -113,3 +218,51 @@ def blur_bbs(im, bbs):
     smooth = cv2.GaussianBlur(im, (5, 5), 0)
     
     return Image.fromarray(np.uint8(smooth))
+#endregion
+
+
+#region Segmentation heat map
+def binarize(im):
+    """Binarize an image into background (0) and foreground (1) using Otsu's method
+    """
+    im_gray = cv2.cvtColor(im, cv2.COLOR_RGB2GRAY)
+    _, im_binary = cv2.threshold(im_gray, 0, 255, cv2.THRESH_OTSU)
+    return im_binary/255
+
+
+def remove_noise(im, kernel=np.ones((3,3), np.uint8), iterations=2):
+    """Remove noise in a binary image via morphological transformations
+    """
+    im_denoise = cv2.morphologyEx(im, cv2.MORPH_OPEN, kernel, iterations)
+    im_denoise = cv2.morphologyEx(im_denoise, cv2.MORPH_CLOSE, kernel, iterations)
+    return im_denoise
+
+
+def segmentation_heat_map(im, bbs, kernel=np.ones((5,5), dtype=np.uint8)):
+    """Converts instance bounding boxes in an image to a single segmentation mask
+
+    Args:
+        im (ndarray): Numpy array of image (colors, height, width)
+    """
+
+    # Initialize mask with all 0s
+    mask = np.zeros(im.shape[:2], dtype=np.float32)
+    
+    for bb in bbs:
+        # Fill in portion of mask associated with bb segment
+        j, i, w, h = bb
+        
+        im_slice = im[i:i+h, j:j+w]
+        im_slice = binarize(im_slice)
+        im_slice = remove_noise(im_slice)
+
+        im_dilated = cv2.dilate(im_slice, kernel, iterations=2)
+        im_eroded = cv2.erode(im_slice, kernel, iterations=2)
+
+        mask_slice = (im_dilated + im_slice + im_eroded)/3
+
+        mask[i:i+h, j:j+w] += mask_slice
+
+    return mask
+#endregion
+
