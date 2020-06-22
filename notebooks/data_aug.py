58293e7019dbf489194f2091a06c3f2d5c6f3498
libertininick <libertini.nick@gmail.com>
2020-06-21 21:04:02 -0600

    Testing loader

--- notebooks/data_aug.py
+++ notebooks/data_aug.py
@@ -71,33 +71,78 @@ for i in range(4):
 
 # ## Load tensors
 
-x, *y, bboxes_aug = loader.load_batch(batch_size=4, resolution_out=256)
-y_pretrained, y_segmentation, y_bboxes, bbox_class_wts = y
+# +
+batch_size=8
+resolution_out=256
+
+(x, *y), (ims_aug, bboxes_aug) = loader.load_batch(batch_size=batch_size, resolution_out=resolution_out)
+y_n_bboxes, y_segmentation, y_bboxes, bbox_class_wts = y
+# -
 
 print(x.shape)
-print(y_pretrained.shape, torch.mean(y_pretrained), torch.std(y_pretrained))
+print(y_n_bboxes.shape, torch.mean(y_n_bboxes), torch.std(y_n_bboxes))
 print(y_segmentation.shape, torch.mean(y_segmentation), torch.std(y_segmentation))
+print(torch.sum(y_segmentation > 0, dim=(1,2,3))/y_n_bboxes.squeeze())
 b, i, j = torch.where(y_bboxes[:, 0, :, :] == 1)
 print(y_bboxes.shape, torch.mean(y_bboxes[b,1:,i,j]), torch.std(y_bboxes[b,1:,i,j]))
-print(bbox_class_wts.shape, torch.sum(bbox_class_wts, dim=(1,2,3)))
+print(bbox_class_wts.shape, torch.sum(bbox_class_wts, dim=(1,2)))
+
+# ### Visualize Augmentations
+
+# +
+fig, axs = plt.subplots(figsize=(20, 10*batch_size), nrows=batch_size, ncols=2)
+
+for i in range(batch_size):
+    _ = axs[i][0].imshow(ims_aug[i])
+    
+    for bb in bboxes_aug[i]:
+        utils.draw_bboxes(axs[i][0], bb)
+    
+    _ = axs[i][1].imshow(y_segmentation[i][0], cmap='gray', vmin=0)
+# -
 
 # ### Centroid mask
 
-fig, ax = plt.subplots(figsize=(10, 10))
-ax.imshow(y_bboxes[0,0,:,:], cmap='gray')
+# +
+fig, axs = plt.subplots(figsize=(20, 10*batch_size), nrows=batch_size, ncols=2)
 
-fig, ax = plt.subplots(figsize=(10, 10))
-ax.imshow(bbox_class_wts[0,0,:,:], cmap='gray')
+for i in range(batch_size):
+    _ = axs[i][0].imshow(ims_aug[i])
+    
+    for bb in bboxes_aug[i]:
+        utils.draw_bboxes(axs[i][0], bb)
+    
+    _ = axs[i][1].imshow(y_bboxes[i][0], cmap='gray', vmin=0, vmax=1)
+# -
 
-i, j = np.where(y_bb_targets[0,0,:,:] == 1)
-y_bb_targets[0,4, i, j]
+# ### Centroid classification wts
+
+# +
+fig, axs = plt.subplots(figsize=(20, 10*batch_size), nrows=batch_size, ncols=2)
+
+for i in range(batch_size):
+    _ = axs[i][0].imshow(y_bboxes[i][0], cmap='gray', vmin=0, vmax=1)
+    _ = axs[i][1].imshow(bbox_class_wts[i]**0.1, cmap='gray')
+# -
 
 # ### `bbox_pred_to_dims`
 
-y_bb_targets[0,1:,i, j][:,0]
-
-utils.bbox_pred_to_dims(*(y_bb_targets[0,1:,i, j][:,-1]), 256, 256)
-
-utils.bbox_pred_to_dims(*[-4, 4, 4, 0], 1024, 1024)
+for b in range(batch_size):
+    bb_true = bboxes_aug[b][np.argsort(bboxes_aug[b][:,0])].astype(np.int64)
+    
+    # Predicted bbs
+    i,j = np.where(y_bboxes[b,0,:,:] == 1)
+    bboxes_pred = [utils.bbox_pred_to_dims(*bbox, resolution_out, resolution_out) 
+                   for bbox 
+                   in y_bboxes[b,1:,i, j].T
+                  ]
+    bboxes_pred = np.array(bboxes_pred)
+    bboxes_pred = bboxes_pred[np.argsort(bboxes_pred[:,0])]
+    
+    print(b, 'Avg Percision:', utils.average_precision(bboxes_pred, bb_true))
+    for idx in range(bboxes_pred.shape[0]):
+        print(f'{idx:>3}', bb_true[idx], bboxes_pred[idx])
+        
+    print()
 
 
